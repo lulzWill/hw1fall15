7537320dc3f8b56587f06140fe914e3e6e46148f
lulzwill <william-pruyn@uiowa.edu>
2015-09-09 16:45:29 +0000

    HW1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,68 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) {|sum,x| x + sum}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).inject(0) {|sum,x| x + sum}
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  !!arr.combination(2).detect{|x,y| n == x + y}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match(/\A[^aeiou\W].*\z/i) ? true : false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s.match(/\A([10]*00|0)\z/) ? true : false
 end
-
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    unless !isbn.empty?
+      raise ArgumentError.new("ISBN cannot be empty")
+    end
+    unless price > 0
+      raise ArgumentError.new("Price cannot be less than zero")
+    end
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn=(x)
+    unless !x.empty?
+      raise ArgumentError.new("ISBN cannot be empty")
+    end
+    @isbn = x
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price=(x)
+    unless x > 0
+      raise ArgumentError.new("Price cannot be less than zero")
+    end
+    @price = x
+  end
+  
+  def price
+    @price
+  end
+  
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
